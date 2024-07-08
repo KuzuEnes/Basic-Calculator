2e344db5b1e77401b3b3ef5539676353f7971b16
Enes Kuzu <eneskuzu2003@gmail.com>
2024-07-08 15:06:14 +0300

    version2

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,55 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
+     public static void main(String[] args){
 
-        System.out.println("Kilonuzu Girin: ");
-        int kilo = scanner.nextInt();
-        System.out.println("Boyunuzu Girin:(Örnek : 1,72) ");
-        double boy = scanner.nextDouble();
+         Scanner scanner = new Scanner(System.in);
+         System.out.println("********************");
+         String islemler = "1. Toplama İşlemi \n"
+                          + "2. Çıkarma İşlemi \n"
+                          + "3. Çarpma İşlemi \n"
+                          + "4. Bölme İşlemi ";
+         System.out.println(islemler);
+         System.out.println("********************");
+         System.out.println("İşlemi seçiniz: ");
+         String islem = scanner.nextLine();
 
-        double bki = kilo / (boy * boy);
-        System.out.println("Beden Kitle Indeksiniz : " + bki);
-        if (bki < 18.5){
-            System.out.println("Beden Kitle Indeksin İdeal Kilonun Altında!!");
-        } else if ((bki > 18.5) && (bki < 24.9)) {
-            System.out.println("Beden Kitle Indeksin İdeal Kiloda!!");
-            
-        } else if ((bki > 25) && (bki < 29.9)) {
-            System.out.println("Beden Kitle Indeksin İdeal Kilonun Üstünde!!");
-            
-        }
-        else if ((bki > 30) && (bki < 39.9)){
-            System.out.println("Beden Kitle Indeksin İdeal Kilonun Çok Üstünde Dikkatli Ol!!");
-        }
-        else {
-            System.out.println("Beden Kitle Indeksin Aşırı Yüksek Kilo Vermelisin!!");
-        }
-    }
+         int a;
+         int b;
+
+         switch (islem){
+             case "1":
+                 System.out.println("Birinci sayı: ");
+                 a = scanner.nextInt();
+                 System.out.println("İkinci sayı: ");
+                 b = scanner.nextInt();
+                 System.out.println("Değerlerin Toplamları : " + (a+b));
+                 break;
+             case "2":
+                 System.out.println("Birinci sayı: ");
+                 a = scanner.nextInt();
+                 System.out.println("İkinci sayı: ");
+                 b = scanner.nextInt();
+                 System.out.println("Değerlerin Farkı : " + (a-b));
+                 break;
+             case "3":
+                 System.out.println("Birinci sayı: ");
+                 a = scanner.nextInt();
+                 System.out.println("İkinci sayı: ");
+                 b = scanner.nextInt();
+                 System.out.println("Değerlerin Çarpımları : " + (a*b));
+                 break;
+             case "4":
+                 System.out.println("Birinci sayı: ");
+                 a = scanner.nextInt();
+                 System.out.println("İkinci sayı: ");
+                 b = scanner.nextInt();
+                 System.out.println("Değerlerin Bölümü : " + ((double)a/b));
+                 break;
+             default:
+                 System.out.println("Geçersiz İşlem...");
+         }
+
+
+     }
 }
